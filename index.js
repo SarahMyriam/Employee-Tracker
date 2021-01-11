5a86e608950d09560fbd101720fd331f01a294c7
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-10 20:21:58 -0500

    add the if statement for the result of the view , add and update employee

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 //require
 const consoleTable = require("console.table");
+const inquirer = require("inquirer");
 
 
 //Responsible for all the prompt
 const db = require("./db");
 
-db.searchAllEmployees().then (
-    function (result){
-        console.table(result);
-    }
-)
-
 //questions 
-const questions = [
-    {
+const questions = [{
     type: "list",
     name: "action",
     message: "What would you like to do?",
-    choices: ["view all employee", "update an employee roles","add an employee",
-            "view all department","add department", "view roles", "add roles"]
+    choices: ["view all employee", "add an employee", "update an employee roles",
+        "view all department", "add department", "view roles", "add roles"
+    ]
 }];
 
 
 // function to initialize program
 function init() {
-//when question is pass to inquerer,
-return inquirer.prompt(questions).then(function(answers){
-    console.log(answers);
+    //when question is pass to inquerer,
+    return inquirer.prompt(questions).then(function (answers) {
+        console.log(answers);
 
-})
+        //if select view all employee
+        if (answers.action === "view all employee") {
+            db.searchAllEmployees().then(
+                function (result) {
+                    console.table(result);
+                }
+            )
+
+        }
+
+        //add employee
+        if (answers.action === "add an employee") {
+            db.searchAllEmployees().then(
+                function (result) {
+                    console.table(result);
+                }
+            )
+
+        }
+
+        //update employee role
+        if (answers.action === "update an employee roles") {
+            db.searchAllEmployees().then(
+                function (result) {
+                    console.table(result);
+                }
+            )
+
+        }
+
+        
+
+        }
+
+    })
 
 }
 // function call to initialize program
